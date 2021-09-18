8e750ade8d5e223b69ad3ee3174eb7482a362503
brinidoss <brinidoss@gmail.com>
2021-09-18 10:54:41 -0400

    changed format of data/ interface

--- functions/src/model/Project.ts
+++ functions/src/model/Project.ts
@@ -1,13 +1,14 @@
 import { ObjectId } from "mongodb";
 
 export default interface Project {
-    category: {
-        name: string;
-        description?: string;
-        label?: string;
-        priority: number;
-        _id?: ObjectId;
-        completed?: boolean;
-        user?: string;
-    }
+    
+    category?: string;
+    name: string;
+    description?: string;
+    label?: string;
+    priority?: number;
+    _id?: ObjectId;
+    completed?: boolean;
+    user?: string;
+    
 }
